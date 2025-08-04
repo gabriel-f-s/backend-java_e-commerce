98e3b56dab194d35b842350fecf2775b28d248e4
gabriel <gabrielmiguel1309@outlook.com>
2025-08-04 15:50:36 -0300

    created the user entity and resource

--- src/main/java/com/user_forge/user_forge/entities/User.java
+++ src/main/java/com/user_forge/user_forge/entities/User.java
@@ -1,4 +1,46 @@
 package com.user_forge.user_forge.entities;
 
-public class User {
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+public class User implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String phone, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+        return Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
 }
--- src/main/java/com/user_forge/user_forge/resources/UserResource.java
+++ src/main/java/com/user_forge/user_forge/resources/UserResource.java
@@ -1,4 +1,18 @@
 package com.user_forge.user_forge.resources;
 
+import com.user_forge.user_forge.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Gabriel", "gabriel@gmail.com", "98765-4321", "senha");
+        return ResponseEntity.ok(u);
+    }
 }
