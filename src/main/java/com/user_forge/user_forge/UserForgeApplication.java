485c44afb61ecac32814efc8d6ba67c8b85d7d4d
gabriel <gabrielmiguel1309@outlook.com>
2025-08-04 12:43:43 -0300

    adding the first files

---
+++ src/main/java/com/user_forge/user_forge/UserForgeApplication.java
@@ -0,0 +1,13 @@
+package com.user_forge.user_forge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserForgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/user_forge/user_forge/UserForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user_forge.user_forge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
